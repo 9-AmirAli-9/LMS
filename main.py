27bebca8db11e155befa605b87a2b1ef002000d3
AmirAli <khoddamamirali841@gmail.com>
2026-06-13 15:34:54 +0330

    fix some issue

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from database import Base , engine , SessionLocal
 from crud import create_book
 from models import Book
-import main_auth
+import auth
 
 
 def run():
     
     print("welcome to markazi library")
-
-    main_auth()
+    Base.metadata.create_all(bind=engine)
+    auth.main_auth()
 
     
+if __name__ == "__main__":
+    run()
